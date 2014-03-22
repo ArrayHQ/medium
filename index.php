cbce61683d88e7ae4691ca1e813694b112321080
John Parris <public@johnparris.com>
2014-03-22 19:31:01 -0400

    Index

--- index.php
+++ index.php
@@ -11,17 +11,23 @@
 							printf( __( '%d results for "%s"', 'medium' ), $wp_query->found_posts, get_search_query( true ) );
 						?>
 					</h2>
-				<?php } else if(is_tag()) { ?>
+
+				<?php } else if( is_tag() ) { ?>
 					<h2 class="archive-title"><?php single_tag_title(); ?></h2>
-				<?php } else if(is_day()) { ?>
-					<h2 class="archive-title"><?php _e('Archive:','medium'); ?> <?php echo get_the_date(); ?></h2>
-				<?php } else if(is_month()) { ?>
-					<h2 class="archive-title"><?php echo get_the_date('F Y'); ?></h2>
-				<?php } else if(is_year()) { ?>
-					<h2 class="archive-title"><?php echo get_the_date('Y'); ?></h2>
-				<?php } else if(is_category()) { ?>
+
+				<?php } else if( is_day() ) { ?>
+					<h2 class="archive-title"><?php _e( 'Archive:', 'medium' ); ?> <?php echo get_the_date(); ?></h2>
+
+				<?php } else if( is_month() ) { ?>
+					<h2 class="archive-title"><?php echo get_the_date( 'F Y' ); ?></h2>
+
+				<?php } else if( is_year() ) { ?>
+					<h2 class="archive-title"><?php echo get_the_date( 'Y' ); ?></h2>
+
+				<?php } else if( is_category() ) { ?>
 					<h2 class="archive-title"><?php single_cat_title(); ?></h2>
-				<?php } else if(is_author()) { ?>
+
+				<?php } else if( is_author() ) { ?>
 					<h2 class="archive-title">
 						<?php
 							the_post();
@@ -35,7 +41,6 @@
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
 				<article <?php post_class('post'); ?>>
-					<!-- uses the post format -->
 					<?php
 						if( 'gallery' == get_post_format() ) {
 							get_template_part( 'format', 'gallery' );
@@ -45,19 +50,18 @@
 					?>
 				</article><!-- post-->
 
-				<?php endwhile; ?>
-				<?php endif; ?>
+				<?php endwhile; endif; ?>
 			</div>
 
-			<?php medium_page_nav(); ?>
+			<?php
+			// Pagination
+			medium_page_nav();
 
-			<!-- comments -->
-			<?php if( is_single () ) { ?>
-				<?php if ('open' == $post->comment_status) { ?>
-					<div id="comment-jump" class="comments">
-						<?php comments_template(); ?>
-					</div>
-				<?php } ?>
+			// Comments
+			if( is_single () && 'open' == $post->comment_status ) { ?>
+				<div id="comment-jump" class="comments">
+					<?php comments_template(); ?>
+				</div>
 			<?php } ?>
 		</div><!-- content -->
 
